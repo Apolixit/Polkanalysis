27177ef83bb23e0d27966440f785db3a1281c959
Apolixit <friotromain@gmail.com>
2023-07-01 21:27:07 +0200

    Fix LogDto ignore by error

---
+++ src/Domain/Polkanalysis.Domain.Contracts/Dto/Logs/LogDto.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polkanalysis.Domain.Contracts.Dto.Logs
+{
+    public class LogDto
+    {
+        public ulong BlockNumber { get; set; }
+        public int LogIndex { get; set; }
+        public Secondary.Pallet.SystemCore.Enums.DigestItem LogType { get; set; }
+        public string ConsensusName { get; set; } = string.Empty;
+        public string DateHex { get; set; } = string.Empty;
+    }
+}
